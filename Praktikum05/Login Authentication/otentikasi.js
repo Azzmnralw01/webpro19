c63d2225c214d8e2aebffde1d26556e986325bae
Azzmnralw01 <72433316+Azzmnralw01@users.noreply.github.com>
2020-11-05 13:31:27 +0700

    Update otentikasi.js

--- Praktikum05/Login Authentication/otentikasi.js
+++ Praktikum05/Login Authentication/otentikasi.js
@@ -3,7 +3,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "ahmad2017" && password == "integrity"){
 alert ("Login successsfully");
-window.location = "test.html";
+window.location = "index.html";
   }
   else{
     alert("Invalid username or password");
